46de3992c3434893741b3e1a5ea3a9b809b94f7b
Gugdit <gugudittricht@gmail.com>
2024-09-17 13:48:49 -0300

    Exercicios das aulas java SENAI

--- Aula 4/exercicio4.java
+++ Aula 4/exercicio4.java
@@ -2,4 +2,5 @@ public class exercicio4 {
     public static void main(String[] args) {
         System.out.println("--------------------Exercício 4: Desconto em compra--------------------");
     }
+
 }
