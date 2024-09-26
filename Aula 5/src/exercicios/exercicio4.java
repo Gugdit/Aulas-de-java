08d5abb188cc8b608f1dce95e4cba789490db7da
Gugdit <gugudittricht@gmail.com>
2024-09-26 13:42:07 -0300

    Exercicios das aulas java SENAI

--- Aula 5/src/exercicios/exercicio4.java
+++ Aula 5/src/exercicios/exercicio4.java
@@ -5,6 +5,6 @@ import java.util.Scanner;
 public class exercicio4 {
     public static void main(String[] args) {
         System.out.println("-------------------Exercício 4: Verificação de Número Primo-------------------");
-        Scanner scanner =
+
     }
 }
