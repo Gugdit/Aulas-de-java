e35f25d57f199345a3e55ae6eb461762c6704197
Gugdit <gugudittricht@gmail.com>
2024-10-22 15:33:54 -0300

    Exercicios das aulas java SENAI

--- src/Aula04/exercicios/exercicio10.java
+++ src/Aula04/exercicios/exercicio10.java
@@ -8,24 +8,29 @@ public class exercicio10 {
         System.out.println("Códigos para a moeda a ser convertida, 1-Dólar, 2-Euro, 3-Libra ");
         System.out.println("Digite um codigo para moedas");
         int codigomoeda = scanner.nextInt();
+        System.out.println("Seu codigo é " +codigomoeda);
         System.out.println("Digite um valor em reais");
         double valorreais = scanner.nextDouble();
-        System.out.println("Seu codigo é " +codigomoeda);
-        switch (codigomoeda) {
-            case 1:
-                double dólares= valorreais * 0.18;
-                System.out.println("Você escolheu dólares seus " + valorreais +" é igual á: "+ dólares +" Dólares");
-                break;
-            case 2:
-                double euros = valorreais * 0.16;
-                System.out.println("Você escolheu euros seus " + valorreais +" é igual á: "+ euros +" Euros");
-                break;
-            case 3:
-                double libras = valorreais * 0.14;
-                System.out.println("Você escolheu libras seus " + valorreais +" é igual á: "+ libras +" Libras");
-                break;
-            default:
-                System.out.println("Esse código não existe, escolha outro.");
+        if (valorreais > 0){
+            switch (codigomoeda) {
+                case 1:
+                    double dólares= valorreais * 0.18;
+                    System.out.println("Você escolheu dólares seus " + valorreais +" é igual á: "+ dólares +" Dólares");
+                    break;
+                case 2:
+                    double euros = valorreais * 0.16;
+                    System.out.println("Você escolheu euros seus " + valorreais +" é igual á: "+ euros +" Euros");
+                    break;
+                case 3:
+                    double libras = valorreais * 0.14;
+                    System.out.println("Você escolheu libras seus " + valorreais +" é igual á: "+ libras +" Libras");
+                    break;
+                default:
+                    System.out.println("Esse código não existe, escolha outro.");
+            }
+        } else if (valorreais < 0) {
+            System.out.println("Valor incorreto, digite denovo.");
+
         }
     }
 }
--- src/Desafio/Desafio.java
+++ src/Desafio/Desafio.java
@@ -5,36 +5,112 @@ import java.util.Scanner;
 public class Desafio {
     public static void main(String[] args) {
         System.out.println("-------------------Desafio!!!!-------------------");
-        System.out.println("--------------------------------------------------"+"\n"+"Escolha uma das opções abaixo "+"\n"+"1-Cadastrar Usuário."+"\n"+"2-Consultar Usuários."+"\n"+"3-Atualizar Usuário."+"\n"+"4-Deletar Usuário."+"\n"+"5-Sair."+"\n"+"--------------------------------------------------");
         Scanner scanner = new Scanner(System.in);
-        int escolhaMenu = scanner.nextInt();
-        switch (escolhaMenu) {
-            case 1:
-                System.out.println("Quantas pessoas você deseja cadastrar?");
-                int numCadastros =scanner.nextInt();
-                scanner.nextLine();
-                String[][] matriz = new String[numCadastros][4];
-                for (int posicao = 0; posicao < numCadastros; posicao++) {
-                     System.out.println("Cadastro da pessoa " + posicao);
-                     System.out.println("ID: ");
-                     matriz[posicao][0] = scanner.nextLine();
 
-                     System.out.println("Nome: ");
-                     matriz[posicao][1] = scanner.nextLine();
+        String[][] matriz = null;
+        boolean entrada = true;
 
-                     System.out.println("E-mail: ");
-                     matriz[posicao][2] = scanner.nextLine();
-
-                     System.out.println("Telefone: ");
-                     matriz[posicao][3] = scanner.nextLine();
-
-                }
-                System.out.println("ID" + "\t" + "Nome" + "\t" + "E-mail" + "\t\t\t" + "Telefone");
-                for (int posicao2 = 0; posicao2 < numCadastros ; posicao2++) {
-                    System.out.println(matriz[posicao2][0] + "\t" + matriz[posicao2][1] + "\t" + matriz[posicao2][2] + "\t" + matriz[posicao2][3]);
-
+        while (entrada) {
+            System.out.println("--------------------------------------------------" + "\n" + "Escolha uma das opções abaixo " + "\n" + "1-Cadastrar Usuário." + "\n" + "2-Consultar Usuários." + "\n" + "3-Atualizar Usuário." + "\n" + "4-Deletar Usuário." + "\n" + "5-Sair." + "\n" + "--------------------------------------------------");
+            int escolhaMenu = scanner.nextInt();
+            scanner.nextLine();
+            switch (escolhaMenu) {
+                case 1:
+                    matriz = cadastrar(matriz);
+                    break;
+                case 2:
+                    consultar(matriz);
+                    break;
+                case 3:
+                    consultar(matriz);
+                    matriz = atualizar(matriz);
+                    break;
+                case 4:
+                    consultar(matriz);
+                    matriz = deletar(matriz);
+                    break;
+                case 5:
+                    entrada = false;
+                    break;
+                default:
+                    System.out.println("Número escolhido não está dentro das opções, escolha outro.");
+                    break;
             }
         }
+
     }
-}
+
+    static String[][] cadastrar(String[][] matriz) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Quantas pessoas você deseja cadastrar?");
+        int numCadastros = scanner.nextInt();
+        scanner.nextLine();
+        matriz = new String[numCadastros][4];
+        for (int posicao = 0; posicao < numCadastros; posicao++) {
+            System.out.println("Cadastro da pessoa " + posicao);
+            System.out.println("ID: ");
+            matriz[posicao][0] = scanner.nextLine();
+
+            System.out.println("Nome: ");
+            matriz[posicao][1] = scanner.nextLine();
+
+            System.out.println("E-mail: ");
+            matriz[posicao][2] = scanner.nextLine();
+
+            System.out.println("Telefone: ");
+            matriz[posicao][3] = scanner.nextLine();
+
+        }
+        return matriz;
+    }
+
+    static String[][] consultar(String[][] matriz) {
+        System.out.println("ID" + "\t" + "Nome" + "\t" + "E-mail" + "\t\t\t" + "Telefone");
+        for (int posicao2 = 0; posicao2 < matriz.length; posicao2++) {
+            System.out.println(matriz[posicao2][0] + "\t" + matriz[posicao2][1] + "\t" + matriz[posicao2][2] + "\t" + matriz[posicao2][3]);
+        }
+        return matriz;
+
+    }
+    static String[][] atualizar(String[][] matriz){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Qual usuário você deseja Atualizar/editar");
+        String editar = scanner.nextLine();
+        for (int posicao3 = 0; posicao3 < matriz.length; posicao3++) {
+            if (editar.equals(matriz[posicao3][0])) {
+                System.out.println("Cadastro da pessoa " + posicao3);
+                System.out.println("ID: ");
+                matriz[posicao3][0] = scanner.nextLine();
+
+                System.out.println("Nome: ");
+                matriz[posicao3][1] = scanner.nextLine();
+
+                System.out.println("E-mail: ");
+                matriz[posicao3][2] = scanner.nextLine();
+
+                System.out.println("Telefone: ");
+                matriz[posicao3][3] = scanner.nextLine();
+            }
+
+        }
+        return matriz;
+    }
+    static String[][] deletar (String[][] matriz){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Qual usuário você deseja apagar?");
+        String deletar = scanner.nextLine();
+        for (int posicao4 = 0; posicao4 < matriz.length; posicao4++) {
+            if (deletar.equals(matriz[posicao4][0])) {
+                matriz[posicao4][0] = "";
+                matriz[posicao4][1] = "";
+                matriz[posicao4][2] = "";
+                matriz[posicao4][3] = "";
+
+                }
+
+            }
+        return matriz;
+
+        }
+    }
 
